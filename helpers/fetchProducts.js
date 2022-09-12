739c3e387f4f8554fe8cc449e1ed28f747cbaa45
Weliton.lima <welitonlima159@gmail.com>
2022-09-12 17:35:07 -0300

    Requisito 3

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,16 +1,20 @@
 const fetchProducts = async (QUERY) => {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
-  const response = await fetch(url);
-  const results = await response.json();
-  const dados = results.results.map((infos) => {
-    const { id, title, thumbnail } = infos;
-    return {
-      id,
-      title,
-      thumbnail,
-    };
-  });
-  return dados;
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+    const response = await fetch(url);
+    const results = await response.json();
+    const dados = results.results.map((infos) => {
+      const { id, title, thumbnail } = infos;
+      return {
+        id,
+        title,
+        thumbnail,
+      };
+    });
+    return dados;
+ } catch (error) {
+  throw new Error('You must provide an url');
+ }
 };
 
 if (typeof module !== 'undefined') {
