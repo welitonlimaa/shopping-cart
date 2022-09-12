a4269aa9decd821a681ae1649a689b270a97b164
Weliton.lima <welitonlima159@gmail.com>
2022-09-12 15:24:51 -0300

    Requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,7 +3,27 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  
-  fail('Teste vazio');
+  it('Verifique se fetchProducts é uma função.', () => {
+    expect.assertions(1);
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Verifique se a função fetch foi chamada.', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledTimes(1);
+  });
+  it('Verifique se a função fetch foi chamada com o endpoint correto', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toBeCalledWith(url);
+  });
+  it('Verifica se ao chamar a função fetchProducts sem argumento retorna um erro', async () => {
+    expect.assertions(1);
+    try {
+      await fetchProducts();
+    } catch (error) { 
+      expect(error).toEqual(new Error('You must provide an url'));
+    }
+  });
 });
